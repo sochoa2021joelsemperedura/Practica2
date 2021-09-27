4377c0cdace4df4ed87767032cb62117d1ef5c93
JoelS <joelsempere96@gmail.com>
2021-09-27 20:04:08 +0200

    Primera version de la práctica 2

---
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica2/MainActivity.java
@@ -0,0 +1,17 @@
+package net.iessochoa.joelsemperedura.practica2;
+/*
+Joel Sempere Durá
+Practica 2
+ */
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
