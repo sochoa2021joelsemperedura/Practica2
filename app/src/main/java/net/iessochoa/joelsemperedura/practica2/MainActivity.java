90f210e9383779f0234d0d8c67e1147495b8556c
Joel Sempere <joelsempere96@gmail.com>
2021-10-05 00:32:27 +0200

    Views comunicadas. SOLUCIONAR PROBLEMA CON IMAGEN DE AGENDA CONSTRAINT Terminar codigo

--- app/src/main/java/net/iessochoa/joelsemperedura/practica2/MainActivity.java
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica2/MainActivity.java
@@ -5,13 +5,41 @@ Practica 2
  */
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAgenda;
+    Button btnAgendaConstraint;
+    Button btnReceta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Iniciamos las views
+        iniciaViews();
+
+        //Creamos el evento del clic que nos lleva a las nuevas views
+        btnAgenda.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this,AgendaActivity.class));
+        });
+        btnAgendaConstraint.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this,AgendaConstraintActivity.class));
+        });
+        btnReceta.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this,RecetaActivity.class));
+        });
+
     }
+
+    //Metodo que inicia las views, vincula los objetos botones creados en nuestra clase con el id que identifica al boton de la view
+    private void iniciaViews() {
+        btnAgenda=findViewById(R.id.btnAgenda);
+        btnAgendaConstraint=findViewById(R.id.btnAgendaConstraint);
+        btnReceta=findViewById(R.id.btnReceta);
+    }
+
 }
