1450cbd7e087653b4ae25061821effad9caa0015
JoelS <joelsempere96@gmail.com>
2021-10-05 18:22:53 +0200

    Opcional1 - Botones vinculados,identificadores de extra, falta crear los eventos de pasar de una view a otra y revisar que esta todo correcto

--- app/src/main/java/net/iessochoa/joelsemperedura/practica2/RecetaActivity.java
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica2/RecetaActivity.java
@@ -2,13 +2,49 @@ package net.iessochoa.joelsemperedura.practica2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RecetaActivity extends AppCompatActivity {
-
+    TextView tvDescripcion, tvDescripcionContenido, tvReceta, tvRecetaContenido, tvIngredientes, tvIngredientesContenido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receta);
+
+        //iniciamos las views
+        iniciaViews();
+
+        tvDescripcionContenido.setOnClickListener(view -> {
+            /*
+        startActivity(
+                new Intent(RecetaActivity.this,TextoActivity.class).putExtra(
+                        TextoActivity.EXTRA_TITULO,tvDescripcion.getText().toString()
+                ));
+        )
+        REVISAR ESTO QUE FUNCIONA PERO FALLA LA SINTAXIS
+             */
+
+        });
+
+        tvRecetaContenido.setOnClickListener(view -> {
+
+        });
+
+        tvIngredientesContenido.setOnClickListener(view -> {
+
+        });
+    }
+    private void iniciaViews(){
+        tvDescripcion=findViewById(R.id.tvDescripcion);
+        tvDescripcionContenido=findViewById(R.id.tvDescripcionContenido);
+
+        tvIngredientes=findViewById(R.id.tvIngredientes);
+        tvIngredientesContenido=findViewById(R.id.tvIngredientesContenido);
+
+        tvReceta=findViewById(R.id.tvReceta);
+        tvRecetaContenido=findViewById(R.id.tvRecetaContenido);
+
     }
 }
---
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica2/TextoActivity.java
@@ -0,0 +1,34 @@
+package net.iessochoa.joelsemperedura.practica2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TextoActivity extends AppCompatActivity {
+/*
+Joel Sempere Durá
+Parte opcional 1 - Activity que abre el texto ampliado de la receta activity ya sea ingredientes
+descripcion o receta.
+
+ */
+    TextView tvTitulo;
+    TextView tvContenido;
+
+    //Etiquetas para los extras
+    public static final String EXTRA_TITULO="TextoActivity.TITULO";
+    public static final String EXTRA_CONTENIDO="TextoActivity.CONTENIDO";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_texto);
+        //Iniciamos views
+        iniciaViews();
+
+        //AHORA AQUI HAY QUE SUSTITUIR EL CONTENIDO POR EL CLICADO EN RECETA
+    }
+    private void iniciaViews(){
+        tvTitulo=findViewById(R.id.tvTitulo);
+        tvContenido=findViewById(R.id.tvContenido);
+    }
+}
