879b9ddec49e18c67eea3484ad6817f7ef166c7b
Joel Sempere <joelsempere96@gmail.com>
2021-10-10 19:37:07 +0200

    PO02- Iniciando parte 2 de la práctica opcional

---
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica2/PeliculaActivity.java
@@ -0,0 +1,14 @@
+package net.iessochoa.joelsemperedura.practica2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PeliculaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pelicula);
+    }
+}
